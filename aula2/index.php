a70d837b544e78882c2d69ea49e9e918139d7d64
Jonathan  Guimarães <guimaraesjonathan92@gmail.com>
2022-09-29 12:20:21 -0300

    Começo de uma nova linguagem

---
+++ aula2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // print e echo sao comandos de " impressao"
+    echo "meu nome é Jonathan <br>";
+    print "minha idade é 20 ";
+    ?>
+</body>
+
+</html>
---
+++ aula3/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// meu dados
+$nome = "Jonathan Guimaraaes";
+$idade = 20;
+$altura = 1.81;
+// para criação de variavel é so por o cifrao e seguir as boas praticas de programacao
+
+echo "<div style='text-align:center;'>Meu nome é $nome,minha idade é $idade e minha altura é $altura </div>";
+// novos valores
+$nome = "Everthon";
+
+$idade = 20;
+$altura = 1.75;
+echo "Meu nome é $nome,minha idade é $idade e minha altura é $altura";
+echo"<hr>";
