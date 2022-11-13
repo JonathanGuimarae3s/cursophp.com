a5aaf3d826c11a00b772e666a7599cd8aac2714a
Jonathan Guimarães <guimaraesjonathan92@gmail.com>
2022-11-12 23:29:59 -0300

    get & post

---
+++ aula18/index2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="dados.php" method="GET">
+
+
+        nome: <input type="text" name="nome" id="">
+        email: <input type="email" name="email" id="">
+        <input type="submit" name="enviar">
+    </form>
+
+<a href="dados.php?idade='25&sobrenome=mark">ENVIAR DADOS</a>
+</body>
+
+</html>
