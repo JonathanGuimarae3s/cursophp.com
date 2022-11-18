4c734c9357890aa17548a1cb7143362db001e489
Jonathan Guimarães <guimaraesjonathan92@gmail.com>
2022-11-18 12:46:52 -0300

    update

--- aula18/index.php
+++ aula18/index.php
@@ -11,28 +11,21 @@
 if (isset($_POST['enviar-formulario'])) {
     // array de erros
     $erros = array();
-    $idade = $_POST['idade'];
-    $email = $_POST['email'];
-    $peso = $_POST['peso'];
-    $ip = $_POST['ip'];
 
-    $url = $_POST['url'];
-    // validações
-    if (!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)) {
-        $erros[] = "Idade precisa ser um inteiro";
-    }
-    if (!$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
-        $erros[] = "email invalido";
-    }
-    if (!$peso = filter_input(INPUT_POST, 'peso', FILTER_VALIDATE_FLOAT)) {
-        $erros[] = "peso precisar ser um numero racional";
-    }
-    if (!$ip = filter_input(INPUT_POST, 'ip', FILTER_VALIDATE_IP)) {
-        $erros[] = "ip invalido";
-    }
-    if (!$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL)) {
-        $erros[] = "url invalida";
-    }
+    // sanitize
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+    $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
+    if (!filter_var($idade, FILTER_VALIDATE_INT)) :
+        $erros[] = 'idade precisa ser um inteiro';
+    endif;
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) :
+        $erros[] = 'email invalido';
+    endif;
+    $url = filter_input(INPUT_POST, 'url', FILTER_SANITIZE_URL);
+    if (!filter_var($url, FILTER_VALIDATE_URL)) :
+        $erros[] = 'url invalida';
+    endif;
     // mensagens de erro 
     if (!empty($erros)) {
         foreach ($erros as $erro) {
@@ -51,8 +44,6 @@ if (isset($_POST['enviar-formulario'])) {
         nome: <input type="text" name="nome" id=""><br>
         idade: <input type="text" name="idade" id=""><br>
         email: <input type="email" name="email" id=""><br>
-        Peso: <input type="text" name="Peso" id=""><br>
-        ip: <input type="text" name="ip" id=""><br>
         url: <input type="text" name="url" id=""><br>
         <button type="submit" name="enviar-formulario">enviar</button>
     </form>
