4e5fecaa268c1ef8057c44af3afdfe68a5314b26
Jonathan Guimarães <guimaraesjonathan92@gmail.com>
2022-12-06 22:09:12 -0300

    multiplos uploads

--- aula19/index.php
+++ aula19/index.php
@@ -8,31 +8,46 @@
     <title>Document</title>
 </head>
 <?php
+// verificando se existe esse indice(enviar-formulario) na super global pos
 if (isset($_POST['enviar-formulario'])) :
+    // setando os formatos permitidos
     $formatosPermitidos = array("png", "jpeg", "jpg", "gif");
-    $extensao = pathinfo($_FILES['arquivos']['name'], PATHINFO_EXTENSION);
-    if (in_array($extensao, $formatosPermitidos)) :
-        $pasta = "arquivos/";
-        $temporario = $_FILES['arquivos']['tmp_name'];
-        $novoNome = uniqid() . ".$extensao";
-        if (move_uploaded_file($temporario, $pasta . $novoNome)) :
-            $mensagem = "upload feito com sucesso";
+    $quantidadeArquivos = count($_FILES['arquivos']['name']);
+    $contador = 0;
+    while ($contador < $quantidadeArquivos) :
+        //verificando se a extensao é valida com a função pathinfo 
+
+        $extensao = pathinfo($_FILES['arquivos']['name'][$contador], PATHINFO_EXTENSION);
+        // verificnado se o valor da extensao existe no array formatosPermitidos com in_array (valor a ser procurado, array que que sera feita a busca)
+        if (in_array($extensao, $formatosPermitidos)) :
+            // add o caminho da img
+            $pasta = "arquivos/";
+            // pegando o caminho do arquivo
+            $temporario = $_FILES['arquivos']['tmp_name'][$contador];
+
+            $novoNome = uniqid() . ".$extensao";
+            // verificando se houve realmente o upload. move_uploaded_file(arquivo a ser movido, pasta em que contera o arquivo movido)
+            if (move_uploaded_file($temporario, $pasta . $novoNome)) :
+                // se for vdd mostra a mensagem
+                echo "upload feito com sucesso";
+
+            else :
+                echo "Nao foi possivel fazer o upload";
+            endif;
 
         else :
-            $mensagem = "Nao foi possivel fazer o upload";
+            echo "$extensao é formato invalido";
         endif;
-
-    else :
-        $mensagem = "formato invalido";
-    endif;
-    echo "$mensagem";
+        $contador++;
+    endwhile;
 endif;
 
 ?>
 
 <body>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-        <input type="file" name='arquivos'><br>
+        <!-- enctype serve para definir o tipo de codificação com que os dados serao enviados -->
+        <input type="file" name='arquivos[]' multiple><br>
         <input type="submit" name="enviar-formulario">
     </form>
 </body>
