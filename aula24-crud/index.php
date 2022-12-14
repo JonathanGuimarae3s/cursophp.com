a61f9f9c29b9f3b3a224731ab86b770a5e674af9
Jonathan Guimarães <guimaraesjonathan92@gmail.com>
2022-12-13 22:31:45 -0300

    inicio do crud

---
+++ aula24-crud/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once 'includes/header.php'; ?>
+.<div class="row justify-content-center align-items-center g-2">
+    <div class="col">Column</div>
+
+</div>
+<?php
+
+include_once 'includes/header.php'; ?>.
